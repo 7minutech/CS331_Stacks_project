fe648cab3e20f3cc227511bf979c86a814ad1c31
7minutech <elijohnson1107@gmail.com>
2024-09-03 22:06:39 -0500

    fix: test now work as intended
    Variable names were wrong and method test need
    for test to be the first word making only 2 tests
    run

--- test/postfix_test.py
+++ test/postfix_test.py
@@ -3,13 +3,13 @@ from lib.postfix import Postfix
 import pytest
 def test_basic_postfix_convert():
     my_postfix = Postfix
-    assert my_postfix.concovert_to_postfix("a + b * c") == "a b c * +"
-def test_successive_precendence_postfix_convert():
+    assert my_postfix.convert_to_postfix("a + b * c") == "a b c * +"
+def test_successive_precedence_postfix_convert():
     my_postfix = Postfix
-    assert my_postfix.concovert_to_postfix("a - b + c") == "a b - c +"
-def first_test_parentheses_postfix_convert():
+    assert my_postfix.convert_to_postfix("a - b + c") == "a b - c +"
+def test_one_parentheses_postfix_convert():
     my_postfix = Postfix
-    assert my_postfix.concovert_to_postfix("(a) a - b + c") == "a b - c +"
-def second_test_parentheses_postfix_convert():
+    assert my_postfix.convert_to_postfix("(a) a - b + c") == "a b - c +"
+def test__two_parentheses_postfix_convert():
     my_postfix = Postfix
-    assert my_postfix.concovert_to_postfix("(b) a ^ b ^ c") == "a b c ^ ^"
+    assert my_postfix.convert_to_postfix("(b) a ^ b ^ c") == "a b c ^ ^"
