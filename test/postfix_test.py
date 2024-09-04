c0ffc1aabd2a82124a779a2aa89a5a4370921440
7minutech <elijohnson1107@gmail.com>
2024-09-03 22:24:08 -0500

    feat: Added tests for evaluate methods

--- test/postfix_test.py
+++ test/postfix_test.py
@@ -1,4 +1,4 @@
-#python -m pytest
+#run command python -m pytest
 from lib.postfix import Postfix
 import pytest
 def test_basic_postfix_convert():
@@ -10,6 +10,15 @@ def test_successive_precedence_postfix_convert():
 def test_one_parentheses_postfix_convert():
     my_postfix = Postfix
     assert my_postfix.convert_to_postfix("(a) a - b + c") == "a b - c +"
-def test__two_parentheses_postfix_convert():
+def test_two_parentheses_postfix_convert():
     my_postfix = Postfix
     assert my_postfix.convert_to_postfix("(b) a ^ b ^ c") == "a b c ^ ^"
+def test_add_multiply_postfix_evaluate():
+    my_postfix = Postfix
+    assert my_postfix.convert_to_postfix("9 + 5 * 3") == 42
+def test_add_sub_parentheses_postfix_convert():
+    my_postfix = Postfix
+    assert my_postfix.convert_to_postfix("6 - 4 + 10") == 12
+def test_parenthesis_parentheses_postfix_convert():
+    my_postfix = Postfix
+    assert my_postfix.convert_to_postfix("3 ^ 3 ^ 2") == 27
