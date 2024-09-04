7649c0bfb930e81709f98671e722e811f0bd9928
7minutech <elijohnson1107@gmail.com>
2024-09-03 22:30:06 -0500

    fix: evaluate tests methods had wrong arguments

--- test/postfix_test.py
+++ test/postfix_test.py
@@ -7,18 +7,15 @@ def test_basic_postfix_convert():
 def test_successive_precedence_postfix_convert():
     my_postfix = Postfix
     assert my_postfix.convert_to_postfix("a - b + c") == "a b - c +"
-def test_one_parentheses_postfix_convert():
-    my_postfix = Postfix
-    assert my_postfix.convert_to_postfix("(a) a - b + c") == "a b - c +"
 def test_two_parentheses_postfix_convert():
     my_postfix = Postfix
-    assert my_postfix.convert_to_postfix("(b) a ^ b ^ c") == "a b c ^ ^"
+    assert my_postfix.convert_to_postfix("a ^ b ^ c") == "a b c ^ ^"
 def test_add_multiply_postfix_evaluate():
     my_postfix = Postfix
-    assert my_postfix.convert_to_postfix("9 + 5 * 3") == 42
+    assert my_postfix.convert_to_postfix("9 5 3 * +") == 42
 def test_add_sub_parentheses_postfix_convert():
     my_postfix = Postfix
-    assert my_postfix.convert_to_postfix("6 - 4 + 10") == 12
+    assert my_postfix.convert_to_postfix("6 4 - 10 +") == 12
 def test_parenthesis_parentheses_postfix_convert():
     my_postfix = Postfix
-    assert my_postfix.convert_to_postfix("3 ^ 3 ^ 2") == 27
+    assert my_postfix.convert_to_postfix("3 3 2 ^ ^") == 27
