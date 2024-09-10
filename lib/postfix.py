0442199082e3abadb0322781350036b94a4d3285
7minutech <elijohnson1107@gmail.com>
2024-09-09 23:07:11 -0500

    feat: added method to set the expression
    Should allow the class to overwrite its expression

--- lib/postfix.py
+++ lib/postfix.py
@@ -2,9 +2,12 @@ class Postfix:
     def __init__(self,expression = None):
         self.expression = expression
 
-    def convert_to_postfix():
+    def convert_to_postfix(self):
         return None
 
     def evaluate_to_postfix():
         return None
+    
+    def set_expression(self, expression):
+        self.expression = expression
 
