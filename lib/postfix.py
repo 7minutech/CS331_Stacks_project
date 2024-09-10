3210a89fa0a6a6b368499112c647fe03be9b284f
7minutech <elijohnson1107@gmail.com>
2024-09-09 23:04:49 -0500

    refactor: class now comes with expression variable
    Should keep up with expression in object as it is
    converted from infix to postfix

--- lib/postfix.py
+++ lib/postfix.py
@@ -1,6 +1,7 @@
 class Postfix:
-    def __init__(self):
-        None
+    def __init__(self,expression = None):
+        self.expression = expression
+
     def convert_to_postfix():
         return None
 
