823b10c535781e19f4d1aa0bcd398b8c34241bad
7minutech <elijohnson1107@gmail.com>
2024-09-09 23:16:32 -0500

    refactor: changed convert and evaluate method
    Should let each be able to work with and without
    an argument b/c then you could pass an expression
    or let the obj hold the expression and use both
    methods on it

--- lib/postfix.py
+++ lib/postfix.py
@@ -2,10 +2,10 @@ class Postfix:
     def __init__(self,expression = None):
         self.expression = expression
 
-    def convert_to_postfix(self):
-        return None
+    def convert_to_postfix(self, expression = None):
+        #"a + b * c" == "a b c * +"
 
-    def evaluate_to_postfix():
+    def evaluate_to_postfix(self, expression = None):
         return None
     
     def set_expression(self, expression):
