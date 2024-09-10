fc783ffdc8d40a409067e30a28f02fa8f13c9ec2
7minutech <elijohnson1107@gmail.com>
2024-09-10 00:30:31 -0500

    passed_test_1: conversion_postfix method

--- lib/postfix.py
+++ lib/postfix.py
@@ -1,12 +1,27 @@
+import pdb
 class Postfix:
     OPERATORS = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3}
     def __init__(self,expression = None):
-        self.expression = expression
+        self.expression = expression.replace(" ","")
 
     def convert_to_postfix(self, expression = None):
         #"a + b * c" == "a b c * +"
+        sym_arr = []
+        operator_arr =[]
         if self.expression is not None:
-            print("case 1")
+            for char in self.expression:
+                if char in self.OPERATORS:
+                    operator_arr.append(char)
+                    if len(operator_arr) > 1:
+                        for i in range(len(operator_arr)- 1):
+                            if self.OPERATORS[operator_arr[i]] > self.OPERATORS[operator_arr[i+1]]:
+                                sym_arr.append(operator_arr.pop())
+                else:
+                    sym_arr.append(char)
+            for i in range(len(operator_arr)):
+                sym_arr.append(operator_arr.pop())
+            return " ".join(sym_arr)
+
         elif expression is not None:
             print("case 2")
         else:
@@ -16,13 +31,3 @@ class Postfix:
     
     def set_expression(self, expression):
         self.expression = expression
-
-    def set_operand_arr(self):
-        operand_arr = []
-        for char in self.expression:
-            if char not in self.OPERATORS:
-                operand_arr.append(char)
-        return operand_arr
-
-
-
