2d17eb867790c9fd94d06d2c012e0628baa8b84b
7minutech <elijohnson1107@gmail.com>
2024-09-10 21:12:45 -0500

    refactor: changed convert method to be only static
    Method must only be static for assignment

--- lib/postfix.py
+++ lib/postfix.py
@@ -1,24 +1,24 @@
 import pdb
 class Postfix:
     OPERATORS = {"+": 1, "-": 1, "*": 2, "/": 2, "^": 3}
-    def __init__(self,expression = None):
-        self.expression = expression.replace(" ","")
 
-    def convert_to_postfix(self, expression = None):
+    @staticmethod
+    def convert_to_postfix(expression):
         #"a + b * c" == "a b c * +"
+        expression = expression.replace(" ","")
         sym_arr = []
         operator_arr =[]
-        if self.expression is not None:
-            for char in self.expression:
-                if char in self.OPERATORS:
-                    operator_arr.append(char)
-                    if len(operator_arr) > 1:
-                        for i in range(len(operator_arr)- 1):
-                            if self.OPERATORS[operator_arr[i]] > self.OPERATORS[operator_arr[i+1]]:
-                                sym_arr.append(operator_arr.pop())
-                            elif self.OPERATORS[operator_arr[i]] == self.OPERATORS[operator_arr[i+1]] and operator_arr[i] != "^":
-                                sym_arr.append(operator_arr[i])
-                                operator_arr.remove(operator_arr[i])
+
+        for char in expression:
+            if char in Postfix.OPERATORS:
+                operator_arr.append(char)
+                if len(operator_arr) > 1:
+                    for i in range(len(operator_arr)- 1):
+                        if Postfix.OPERATORS[operator_arr[i]] > Postfix.OPERATORS[operator_arr[i+1]]:
+                            sym_arr.append(operator_arr.pop())
+                        elif Postfix.OPERATORS[operator_arr[i]] == Postfix.OPERATORS[operator_arr[i+1]] and operator_arr[i] != "^":
+                            sym_arr.append(operator_arr[i])
+                            operator_arr.remove(operator_arr[i])
 
                 else:
                     sym_arr.append(char)
@@ -26,12 +26,7 @@ class Postfix:
                 sym_arr.append(operator_arr.pop())
             return " ".join(sym_arr)
 
-        elif expression is not None:
-            print("case 2")
-        else:
-            print("case 3")
     def evaluate_to_postfix(self, expression = None):
         return None
     
-    def set_expression(self, expression):
-        self.expression = expression
+    
