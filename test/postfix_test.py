96979f6ed9560aa9f56f307987ed29b48a5afd08
7minutech <elijohnson1107@gmail.com>
2024-09-10 23:15:36 -0500

    feat: added test for convert method
    New test, test for an expression containing
    parenthesis

--- test/postfix_test.py
+++ test/postfix_test.py
@@ -15,6 +15,10 @@ def test_powers_postfix_convert():
 
 def test_multiply_add_postfix_convert():
     assert Postfix.convert_to_postfix("a * b + c") == "a b * c +"
+
+def test_parenthesis_postfix_convert():
+        assert Postfix.convert_to_postfix("(a + b) * c") == "a b * c +"
+
 '''
 def test_add_multiply_postfix_evaluate():
     my_postfix = Postfix()
