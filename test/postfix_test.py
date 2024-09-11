970a3e88494393d77abb572c7d5dda540779a27a
7minutech <elijohnson1107@gmail.com>
2024-09-10 23:48:43 -0500

    fix: fixed test typo

--- test/postfix_test.py
+++ test/postfix_test.py
@@ -17,7 +17,7 @@ def test_multiply_add_postfix_convert():
     assert Postfix.convert_to_postfix("a * b + c") == "a b * c +"
 
 def test_parenthesis_postfix_convert():
-        assert Postfix.convert_to_postfix("(a + b) * c") == "a b * c +"
+        assert Postfix.convert_to_postfix("(a + b) * c") == "a b + c *"
 
 '''
 def test_add_multiply_postfix_evaluate():
